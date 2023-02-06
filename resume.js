9538f83b506f9b7407b9e77c6b8322304a2a48ee
gowthamhari12 <103361689+gowthamhari12@users.noreply.github.com>
2023-02-06 15:44:04 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,53 @@
+let myResume={
+    "basicDetails":{
+        "name":"GOWTHAM B",
+    "email":"gowthamhari46@gmail.com",
+    "phone":7904190839,
+    "degree":"B.E(MECHANICAL)",
+    "location":{
+        "address":"11 New Goldencity, Panikkampatti, Pollachi ",
+        "postalcode":642002,
+        "city":"pollachi",
+        "state":"tamilnadu",
+        "country":"india"
+    },
+    "work":[
+        {
+            "company":"RBL FINSERVE LTD",
+            "position":"Relationship Officer",
+            "startDate":"2018-05-02",
+            "endDate":"2019-11-04",
+            "summary":"Hello I am  the Relationship Officer handling the MSME loan sales and recovery",
+        }
+    ],
+
+    "education":[
+        {
+            "College":"Karpagam Academy of Higher Education",
+            "department":"MECHANICAL ENGINEERING",
+            "study type":"partTime",
+            "batch start year":2019,
+            "batch end year":2022,
+            "cgpa":8.7,
+        }
+    ],
+
+    "skill":[
+        {
+            "name":"Mechiune Drawing knowlage, Gym trainning, Auto Cad, solid modeling, HTML, CSS, JAVA SCRIPT,",
+            "level":"Expert",
+            "project":[
+                "EXPERIMENTAL  INVESTIGATION ON DIMPLE TEXTURED SINGLE POINT CUTTING TOOL"
+            ]
+        }
+    ],
+    "languages":["Tamil, English"],
+    "interests": [
+        {
+            "name":["Web Devlopment", "Fitness"],
+        }
+    ],
+
+    }
+}
+console.log(myResume);
